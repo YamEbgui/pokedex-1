b627d5757fcb0825fb97ab2e13420d74d2b788cf
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-10-14 21:12:15 +0300

    Showing more pokemon's from same type

--- src/index.js
+++ src/index.js
@@ -20,6 +20,17 @@ async function getPokemon(name) {
 	}
 }
 
+//get a list of all the pokemon's from the same type
+async function getPokemonsOfType(type) {
+	try {
+		return await axios
+			.get(`https://pokeapi.co/api/v2/type/${type}`)
+			.then((response) => response.data.pokemon);
+	} catch (error) {
+		//if no more pokemon's from this type exist
+		alert(`There are no more Pokemon's from type: ${type}`);
+	}
+}
 /**  Event Listeners **/
 
 searchButton.addEventListener(`click`, async () => {
@@ -28,6 +39,7 @@ searchButton.addEventListener(`click`, async () => {
 		pokemon = await getPokemon(searchInput.value);
 		addPokemonToPage(pokemon);
 		searchInput.value = ``;
+		addTypeList(pokemon.types);
 	} else {
 		alert(`Please enter a Pokemon name or ID`);
 	}
@@ -57,4 +69,31 @@ function addPokemonToPage(pokemon) {
 	height.replaceChildren(document.createTextNode(pokemon.height));
 	weight.replaceChildren(document.createTextNode(pokemon.weight));
 	image.setAttribute('src', pokemon.sprites.front_default);
+}
+
+//create a list of pokemon's
+function createPokemonsList(pokemons) {
+	const pokemonsList = document.createElement(`ul`);
+
+	for (const pokemon of pokemons) {
+		let pokemonLiElement = document.createElement(`li`);
+		pokemonLiElement.innerHTML = pokemon.pokemon.name;
+		pokemonsList.appendChild(pokemonLiElement);
+	}
+	return pokemonsList;
+}
+
+//add a list of pokemon's from the same type
+async function addTypeList(types) {
+	document.getElementById('type-list').innerHTML = ''; //reset list
+
+	for (const type of types) {
+		//add a title for the list
+		const listHeading = document.createElement(`h3`);
+		listHeading.innerText = `More "${type.type.name}" type Pokemon's:`;
+		document.getElementById('type-list').appendChild(listHeading);
+		const pokemonsListObject = await getPokemonsOfType(type.type.name);
+		const list = createPokemonsList(pokemonsListObject);
+		document.getElementById('type-list').appendChild(list);
+	}
 }
